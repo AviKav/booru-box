97056322c2562e881282d6a75df4b1a604c5fd86
AviKav <avikav@avikav.net>
2022-03-21 02:06:59 -0400

    add config/*.secret.exs

---
+++ config/dev.secret.example.exs
@@ -0,0 +1,9 @@
+use Mix.Config
+
+# config :booru_box, BooruBox.Repo,
+  # username: "username",
+  # password: "password",
+  # hostname: "localhost",
+  # port: 5432,
+  # database: "booru_box_dev",
+  #socket_dir: "/tmp" # If connecting by unix socket
